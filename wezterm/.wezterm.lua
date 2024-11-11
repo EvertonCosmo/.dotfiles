55e3ca9772435c920ca8ca7dd0a34f65760f4746
Everton Cosmo <everton8cosmo@gmail.com>
2024-11-11 07:33:19 -0300

    feat: add wezterm spacing

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,8 +4,6 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-
-
 config.adjust_window_size_when_changing_font_size = false
 
 -- This is where you actually apply your config choices
@@ -13,16 +11,18 @@ config.adjust_window_size_when_changing_font_size = false
 -- For example, changing the color scheme:
 -- config.color_scheme = "Abernathy"
 config.font = wezterm.font("Comic Code Ligatures")
-config.font_size = 13 
+-- config.font_size = 12.5
+-- config.font_size = 12.6
+
 config.color_scheme = "Tokyo Night"
 
 config.enable_tab_bar = false
 
 config.window_padding = {
-  top = 0,
-  left = 5,
-  right = 0,
-  bottom = 0,
+	top = 0,
+	left = 5,
+	right = 0,
+	bottom = 0,
 }
 
 -- and finally, return the configuration to wezterm
