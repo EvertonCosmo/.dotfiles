83a558adbe3cc0e91786023c1a6a1767ec9c3cc9
Everton Cosmo <everton8cosmo@gmail.com>
2025-04-13 14:42:56 -0300

    feat: add rose-pine default

--- nvim/.config/nvim/lua/everton/plugins/rose-pine.lua
+++ nvim/.config/nvim/lua/everton/plugins/rose-pine.lua
@@ -1,18 +1,19 @@
--- thanks to https://github.com/Sirikakire/
-
 return {
 	"rose-pine/neovim",
-	priority = 100,
+	name = "rose-pine",
 	config = function()
 		require("rose-pine").setup({
-
+			variant = "auto", -- auto, main, moon, or dawn
+			dark_variant = "main", -- main, moon, or dawn
 			dim_inactive_windows = false,
-			extend_background_behind_borders = false,
+			extend_background_behind_borders = true,
+
 			enable = {
 				terminal = true,
-				legacy_highlights = true,
-				migrations = true,
+				legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+				migrations = true, -- Handle deprecated options automatically
 			},
+
 			styles = {
 				bold = true,
 				italic = false,
@@ -50,9 +51,21 @@ return {
 				-- h6 = "foam",
 			},
 
+			palette = {
+				-- Override the builtin palette per variant
+				-- moon = {
+				--     base = '#18191a',
+				--     overlay = '#363738',
+				-- },
+			},
+
+			-- NOTE: Highlight groups are extended (merged) by default. Disable this
+			-- per group via `inherit = false`
 			highlight_groups = {
 				-- Comment = { fg = "foam" },
+				-- StatusLine = { fg = "love", bg = "love", blend = 15 },
 				-- VertSplit = { fg = "muted", bg = "muted" },
+				-- Visual = { fg = "base", bg = "text", inherit = false },
 			},
 
 			before_highlight = function(group, highlight, palette)
@@ -68,8 +81,9 @@ return {
 			end,
 		})
 
-		-- vim.cmd([[colorscheme rose-pine]])
-		vim.cmd("colorscheme rose-pine-moon")
 		-- vim.cmd("colorscheme rose-pine")
+		-- vim.cmd("colorscheme rose-pine-main")
+		vim.cmd("colorscheme rose-pine-moon")
+		-- vim.cmd("colorscheme rose-pine-dawn")
 	end,
 }
