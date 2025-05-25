9feec3b76f5a8743ec1eab828de453726ca02fac
EvertonCosmo <everton8cosmo@gmail.com>
2025-05-25 19:57:27 -0300

    fix: inlay hint big width

--- nvim/.config/nvim/lua/everton/plugins/inlay-hints.lua
+++ nvim/.config/nvim/lua/everton/plugins/inlay-hints.lua
@@ -3,6 +3,19 @@ return {
 	event = "LspAttach",
 	dependencies = { "neovim/nvim-lspconfig" },
 	config = function()
-		require("inlay-hints").setup()
+		require("inlay-hints").setup({
+			eol = {
+				parameter = {
+					separator = ", ",
+					format = function(hint)
+						local max_length = 20
+						if #hint > max_length then
+							return hint:sub(1, max_length) .. "..."
+						end
+						return hint
+					end,
+				},
+			},
+		})
 	end,
 }
