b09431a286b5ee8f47bbf2c7f7837bba039763bf
EvertonCosmo <everton8cosmo@gmail.com>
2025-05-25 19:57:40 -0300

    feat: activate tokyonight-night as default color :)

--- nvim/.config/nvim/lua/everton/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/everton/plugins/colorscheme.lua
@@ -3,12 +3,16 @@ return {
 		"folke/tokyonight.nvim",
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
+			-- require("tokyonight").setup({
+			-- 	style = "night",
+			-- 	terminal_colors = true,
+			-- 	transparent = true,
+			-- 	floats = "dark",
+			-- 	sidebars = "dark",
+			-- })
 			require("tokyonight").setup({
 				style = "night",
 				terminal_colors = true,
-				transparent = true,
-				floats = "dark",
-				sidebars = "dark",
 			})
 			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
