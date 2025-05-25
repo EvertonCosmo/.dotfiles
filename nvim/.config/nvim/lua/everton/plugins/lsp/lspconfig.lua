ee34ce4d11b7c640ce4b9efc6b5f652c33ebd4d8
EvertonCosmo <everton8cosmo@gmail.com>
2025-05-25 19:58:09 -0300

    fix: fix and remove some redudant actions
    - update deprecated functions

--- nvim/.config/nvim/lua/everton/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/everton/plugins/lsp/lspconfig.lua
@@ -44,24 +44,29 @@ return {
 				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
 				opts.desc = "Go to previous diagnostic"
-				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+				keymap.set("n", "[d", function()
+					vim.diagnostic.jump({ count = -1, float = true })
+				end, opts) -- jump to previous diagnostic in buffer
 
 				opts.desc = "Go to next diagnostic"
-				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+				keymap.set("n", "]d", function()
+					vim.diagnostic.jump({ count = 1, float = true })
+				end, opts) -- jump to next diagnostic in buffer
 
 				opts.desc = "Show documentation for what is under cursor"
 				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
+				opts.desc = "Show documentation for what is under cursor"
+				keymap.set("n", "K", function()
+					require("pretty_hover").hover()
+				end, opts)
+
 				opts.desc = "Restart LSP"
 				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 
-				keymap.set("i", "<C-h>", function()
-					vim.lsp.buf.signature_help()
-				end, opts)
-
-				keymap.set("n", "<leader>vca", function()
-					vim.lsp.buf.code_action()
-				end, opts)
+				-- keymap.set("i", "<C-h>", function()
+				-- 	vim.lsp.buf.signature_help()
+				-- end, opts)
 			end,
 		})
 
