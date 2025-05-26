0d7fd2f4604212cc9e0c4e830c9bebe7bcc09921
EvertonCosmo <everton8cosmo@gmail.com>
2025-05-25 23:49:23 -0300

    feat: add catppucin

--- nvim/.config/nvim/lua/everton/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/everton/plugins/colorscheme.lua
@@ -1,22 +1,33 @@
 return {
 	{
-		"folke/tokyonight.nvim",
-		priority = 1000, -- make sure to load this before all the other start plugins
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
 		config = function()
-			-- require("tokyonight").setup({
-			-- 	style = "night",
-			-- 	terminal_colors = true,
-			-- 	transparent = true,
-			-- 	floats = "dark",
-			-- 	sidebars = "dark",
-			-- })
-			require("tokyonight").setup({
-				style = "night",
-				terminal_colors = true,
+			require("catppuccin").setup({
+				flavour = "mocha",
 			})
-			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-			vim.cmd([[colorscheme tokyonight]])
+			vim.cmd([[colorscheme catppuccin]])
 		end,
 	},
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	-- priority = 1000, -- make sure to load this before all the other start plugins
+	-- 	config = function()
+	-- 		-- require("tokyonight").setup({
+	-- 		-- 	style = "night",
+	-- 		-- 	terminal_colors = true,
+	-- 		-- 	transparent = true,
+	-- 		-- 	floats = "dark",
+	-- 		-- 	sidebars = "dark",
+	-- 		-- })
+	-- 		require("tokyonight").setup({
+	-- 			style = "night",
+	-- 			terminal_colors = true,
+	-- 		})
+	-- 		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	-- 		-- vim.cmd([[colorscheme tokyonight]])
+	-- 	end,
+	-- },
 }
