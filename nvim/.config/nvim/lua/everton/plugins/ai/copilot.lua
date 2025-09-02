61a7ba2c2dcbe0cfc648c20515a9e27eb9d4b6bd
Everton Cosmo <everton8cosmo@gmail.com>
2025-09-01 23:47:00 -0300

    fix: remove old stuff

--- nvim/.config/nvim/lua/everton/plugins/ai/copilot.lua
+++ nvim/.config/nvim/lua/everton/plugins/ai/copilot.lua
@@ -5,14 +5,12 @@ return {
 		enabled = false,
 		event = "InsertEnter",
 		opts = {
-			copilot_model = "gpt-4o-copilot",
 			node_command = "node",
 			server_opts_overrides = {},
 			suggestion = {
 				enabled = true,
 				auto_trigger = true,
 				hide_during_completion = true,
-				debounce = 75,
 				keymap = {
 					accept = "<tab>",
 					accept_word = false,
