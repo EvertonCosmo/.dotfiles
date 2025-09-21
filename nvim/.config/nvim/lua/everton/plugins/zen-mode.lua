b1911ebee71f957279728784dfb16021f9237fcf
Everton Cosmo <everton8cosmo@gmail.com>
2025-09-21 19:10:23 -0300

    fix: remove incline from zen mode

--- nvim/.config/nvim/lua/everton/plugins/zen-mode.lua
+++ nvim/.config/nvim/lua/everton/plugins/zen-mode.lua
@@ -10,12 +10,12 @@ return {
 				gitsigns = { enabled = true },
 				tmux = { enabled = true },
 			},
-			on_close = function()
-				require("incline").enable()
-			end,
-			on_open = function()
-				require("incline").disable()
-			end,
+			-- on_close = function()
+			-- 	require("incline").enable()
+			-- end,
+			-- on_open = function()
+			-- 	require("incline").disable()
+			-- end,
 		},
 		keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
 	},
